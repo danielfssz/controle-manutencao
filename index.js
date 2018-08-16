d4c530f76d6ffc833648475500042ded94171a11
Daniel Souza <danielfss98@gmail.com>
2018-08-16 19:14:58 -0300

    Alterando criacao tabela

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
 var arrEquipamentos = [
     { nome: 'epp1', usoSemanal: 120, usoTotal: 0, proxLL: 0, proxLLR: 0 },
-    { nome: 'epp1', usoSemanal: 90, usoTotal: 0, proxLL: 0, proxLLR: 0 },
-    { nome: 'epp1', usoSemanal: 95, usoTotal: 0, proxLL: 0, proxLLR: 0 }];
+    { nome: 'epp2', usoSemanal: 90, usoTotal: 0, proxLL: 0, proxLLR: 0 },
+    { nome: 'epp3', usoSemanal: 95, usoTotal: 0, proxLL: 0, proxLLR: 0 }];
 
-
-
+$("#tblManutencao thead tr").append('<th scope="col" style="width: 50px;">Equipamento</th>');
 
 for (let semana = 1; semana <= 53; semana++) {
+    $("#tblManutencao thead tr").append('<th scope="col" style="width: 50px;">Semana' + semana + '</th>');
     for (let cEqp = 0; cEqp < arrEquipamentos.length; cEqp++) {
-        arrEquipamentos[cEqp].usoTotal += arrEquipamentos[cEqp].usoSemanal;
-        arrEquipamentos[cEqp].proxLL += arrEquipamentos[cEqp].usoSemanal;
-        arrEquipamentos[cEqp].proxLLR += arrEquipamentos[cEqp].usoSemanal;
-        if (arrEquipamentos[cEqp].proxLL >= 250) {
-            console.log('o equipamento ' + arrEquipamentos[cEqp].nome + ' entrou para lavagem e lubrificação');
-            arrEquipamentos[cEqp].proxLL = 0;
-        }
-        if (arrEquipamentos[cEqp].proxLLR >= 500) {
-            console.log('o equipamento ' + arrEquipamentos[cEqp].nome + ' entrou para lavagem e lubrificação e Revisao');
-            arrEquipamentos[cEqp].proxLLR = 0;
-        }
+        $("#tblManutencao tbody").append('<tr>');
+        $("#tblManutencao tbody").append('<th scope="col" style="width: 50px;">' + arrEquipamentos[cEqp].nome + '</th>');
+        $("#tblManutencao tbody").append('<td>Otto</td>');
+        $("#tblManutencao tbody").append('</tr>');
     }
 }
 
 
-for (let cEqp = 0; cEqp < arrEquipamentos.length; cEqp++) {
-    console.log('o uso total foi ' + arrEquipamentos[cEqp].usoTotal);
-}
+// for (let cEqp = 0; cEqp < arrEquipamentos.length; cEqp++) {
+//     var linhaEqp = '<tr><th scope="row">' + arrEquipamentos[cEqp].nome + '</td>';
+//     console.log(arrEquipamentos[cEqp].nome)
+//     //cria linha do equipamento
+//     $("#tblManutencao tbody").append(linhaEqp);
+
+//     for (let semana = 1; semana <= 53; semana++) {
+//         //cria coluna da primeira semana
+//         $("#tblManutencao thead").append('<th scope="col" style="width: 50px;"> Semana' + semana + '</th>');
+//         arrEquipamentos[cEqp].usoTotal += arrEquipamentos[cEqp].usoSemanal;
+//         arrEquipamentos[cEqp].proxLL += arrEquipamentos[cEqp].usoSemanal;
+//         arrEquipamentos[cEqp].proxLLR += arrEquipamentos[cEqp].usoSemanal;
+//         //$("#tblManutencao thead").append('<th scope="col" style="width: 50px;"> Semana' + semana + '</th>');
+//         if (arrEquipamentos[cEqp].proxLL >= 250) {
+//             //escreve na celula correspondente se aquele eqp vai pra manutencao
+//             //console.log('o equipamento ' + arrEquipamentos[cEqp].nome + ' entrou para lavagem e lubrificação');
+//             $("#tblManutencao tbody").append('<td>LL</td>');
+//             arrEquipamentos[cEqp].proxLL = 0;
+//         } else {
+//             $("#tblManutencao tbody").append('<td></td>');
+//         }
+//         if (arrEquipamentos[cEqp].proxLLR >= 500) {
+//             //escreve na celula correspondente se aquele eqp vai pra manutencao
+//             //console.log('o equipamento ' + arrEquipamentos[cEqp].nome + ' entrou para lavagem e lubrificação e Revisao');
+//             $("#tblManutencao tbody").append('<td>LLR</td>');
+//             arrEquipamentos[cEqp].proxLLR = 0;
+//         } else {
+//             $("#tblManutencao tbody").append('<td></td>');
+//         }
+//     }
+//     $("#tblManutencao thead").append('</tr>');
+//     $("#tblManutencao tbody").append('</tr>');
+// }
+
